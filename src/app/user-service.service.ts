56a5ebd91d2e0e4cf6dfae288e7f88ecd2cccef5
Caleb Odinga <calemasanga@gmail.com>
2021-05-22 12:45:07 +0300

    adds service for user clss

--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'; 
+import { UserClass } from './user-class';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
 
-  constructor() { }
+  constructor(private HttpCliet:HttpClient) {
+    
+   }
 }
