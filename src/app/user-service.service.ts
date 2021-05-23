391d74a2c5dcda19c9b77a851fbaaeaf5c94f709
Caleb Odinga <calemasanga@gmail.com>
2021-05-23 23:40:00 +0300

    adds initial instances to user and repository

--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -1,13 +1,19 @@
-import { Injectable } from '@angular/core'; 
+import { Injectable } from '@angular/core';
 import { UserClass } from './user-class';
-import {HttpClient} from '@angular/common/http';
+import { RepoClass } from './repo-class';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
+  user!: UserClass;
+  repository!: RepoClass;
+  repositoryData = [];
 
-  constructor(private HttpClient:HttpClient) {
-    
-   }
+
+  constructor(private http: HttpClient) {
+    this.user = new UserClass("", "", 0, 0, 0, "", "");
+    this.repository = new RepoClass('', '', '', '', '', 0, 0);
+  }
 }
