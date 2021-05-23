daf36a198a195eefdca694bb2f943551c18d3aa5
Caleb Odinga <calemasanga@gmail.com>
2021-05-23 23:27:21 +0300

    goes through creation of classes to make sure they work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
-import { AppComponent } from './app.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { UserResultsComponent } from './user-results/user-results.component';
+import { UserSearchComponent } from './user-search/user-search.component';
 
 const routes: Routes = [
-  {path:'home', component:AppComponent},
+  {path:'home', component:UserSearchComponent},
   {path:'about', component:AboutComponent},
   {path:'results', component:UserResultsComponent},
   {path:'', redirectTo:'/home', pathMatch:'full'},
