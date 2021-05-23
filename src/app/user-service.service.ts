e46920c50c5539aef11cb5041680f2bc8c7bfc02
Caleb Odinga <calemasanga@gmail.com>
2021-05-24 00:13:55 +0300

    adds user response to service

--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { UserClass } from './user-class';
 import { RepoClass } from './repo-class';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -26,5 +27,20 @@ export class UserServiceService {
       location: string,
       url: string,
     }
+
+    let promise = new Promise<void>((resolve, reject) => {
+      this.http.get<ApiResponse>(environment.apiurl + userName).toPromise().then(response => {
+        this.user.login = response.login;
+        this.user.name = response.name;
+        this.user.id = response.id;
+        this.user.followers = response.followers;
+        this.user.following = response.following;
+        this.user.location = response.location;
+        this.user.url = response.url;
+
+        resolve()
+      }
+      )
+    })
   }
 }
